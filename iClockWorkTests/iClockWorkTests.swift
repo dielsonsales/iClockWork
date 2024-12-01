2291a19936148d7241bd6122967821b9af962815
Dielson Sales de Carvalho <dielsonscarvalho@gmail.com>
2024-12-01 11:47:35 -0300

    Initial Commit

---
+++ iClockWorkTests/iClockWorkTests.swift
@@ -0,0 +1,17 @@
+//
+//  iClockWorkTests.swift
+//  iClockWorkTests
+//
+//  Created by Dielson Sales de Carvalho on 01/12/24.
+//
+
+import Testing
+@testable import iClockWork
+
+struct iClockWorkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
